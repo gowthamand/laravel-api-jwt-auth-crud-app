9ec81e9d13ab2b88b2968d60bbeec3ca0d993570
Gowthaman <gowthaman.nkl1@gmail.com>
2019-06-29 13:06:29 +0530

    First commit

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+// Cross Origin
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// Login
+Route::post('login', 'UsersController@authenticate');
+Route::post('authenticate', 'UsersController@authenticate');
+// Register
+Route::post('register', 'UsersController@register');
+
+Route::get('posts', 'PostController@index');
+Route::get('posts/index', 'PostController@index');
+Route::post('posts/add', 'PostController@add');
+
+Route::get('employees', 'EmployeesController@index');
+Route::post('employees', 'EmployeesController@store');
+Route::get('employees/{id}', 'EmployeesController@show');
+Route::put('employees/{id}', 'EmployeesController@update');
+Route::delete('employees/{id}', 'EmployeesController@destroy');
+
+
+
+// Auth::routes();
+// Route::get('/home', 'HomeController@index')->name('home');
